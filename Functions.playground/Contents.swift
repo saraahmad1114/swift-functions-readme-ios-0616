e8f63ea4f5283d1f262aeb70a052906f243cccce
Sara <saraahmad1114@yahoo.com>
2016-07-06 10:40:52 -0400

    functions

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -9,6 +9,9 @@ sayHello()
 // Uncomment this line to see the error that is printed in the console.
 // (You can uncomment a line by removing the // at the beginning of the line.)
 //print(greeting)
+sayHello()
+sayHello()
+//in order to call greetings, you need to call the function by using the name of the function
 
 
 func sayHelloToGarfield() {
@@ -32,6 +35,8 @@ sayHelloToCat("Mittens")
 
 let catFriend = "Socks"
 sayHelloToCat(catFriend)
+
+
 
 // Uncomment this line to see the error that is printed in the console.
 //sayHelloToCat()
